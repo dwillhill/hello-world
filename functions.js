628579f67fc93f6d8c7288faa76f6ecac49ed3c5
dwillhill <diego.galatzan@williamhill.com>
2016-06-23 14:44:11 +0300

    Update from github

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 module.exports.helloWorld = function(){
 
-  console.log('Hello World!')
+  console.log('Hello Worldoobydu!')
   console.log('I also wanted to add some more text \n' +
 'and log the result of 2+2 which is: \n' +
 (2+2));
